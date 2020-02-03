84f3a8b38010a794dfe610ff7459d905241818f6
Ellen Cianflone <ellencianflone@gmail.com>
2020-02-03 18:31:18 -0500

    local pretty button9

---
+++ scripts.js
@@ -0,0 +1,52 @@
+
+var date=moment()
+
+document.getElementById("date").innerHTML = date.format("MMMM DD, YYYY h:MM");
+console.log(date.format)
+
+    $("#save9").on("click",
+    function () {
+     var t9 = $("#t9").val();
+     //console.log(t9)
+
+     localStorage.setItem("task9",t9);
+            })
+            
+                //$("#t10").val(t10);
+
+                
+                //$("#t11").val(t11);
+
+                //$("#t12").val(t12);
+
+                //$("#t1").val(t1);
+
+                //$("#t2").val(t2);
+
+                //$("#t3").val(t3);
+
+                //$("#t4").val(t4);
+
+                //$("#t5").val(t5);
+
+               
+               // localStorage.setItem("", t10.value);
+                //localStorage.setItem("", t11.value);
+                //localStorage.setItem("", t12.value);
+                //localStorage.setItem("", t1.value);
+                //localStorage.setItem("", t2.value);
+                //localStorage.setItem("", t3.value);
+                //localStorage.setItem("", t4.value);
+                //localStorage.setItem("", t5.value);
+
+
+           
+
+
+
+
+
+
+
+
+
