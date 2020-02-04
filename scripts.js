5dcc4a5dd408089a024b20731b25f8bd74d0727d
Ellen Cianflone <ellencianflone@gmail.com>
2020-02-04 00:02:41 -0500

    local pretty button9

--- scripts.js
+++ scripts.js
@@ -1,52 +1,72 @@
 
-var date=moment()
-
+var date = moment();
 document.getElementById("date").innerHTML = date.format("MMMM DD, YYYY h:MM");
-console.log(date.format)
+console.log(date);
 
-    $("#save9").on("click",
-    function () {
-     var t9 = $("#t9").val();
-     //console.log(t9)
 
-     localStorage.setItem("task9",t9);
+$("#t9").val(localStorage.getItem("task9"));
+$("#t10").val(localStorage.getItem("task10"));
+$("#t11").val(localStorage.getItem("task11"));
+$("#t12").val(localStorage.getItem("task12"));
+$("#t1").val(localStorage.getItem("task1"));
+$("#t2").val(localStorage.getItem("task2"));
+$("#t3").val(localStorage.getItem("task3"));
+$("#t4").val(localStorage.getItem("task4"));
+$("#t5").val(localStorage.getItem("task5"));
+
+
+$("#save9").on("click",
+    function () 
+    {
+        var t9 = $("#t9").val();
+        localStorage.setItem("task9", t9);
+    })
+        $("#save10").on("click",
+            function () {
+                var t10 = $("#t10").val();
+                localStorage.setItem("task11", t10);
             })
-            
-                //$("#t10").val(t10);
+                $("#save11").on("click",
+                    function () {
+                        var t11 = $("#t11").val();
+                        localStorage.setItem("task11", t11);
+                    })
 
-                
-                //$("#t11").val(t11);
+                        $("#save12").on("click",
+                            function () {
+                                var t12 = $("#t12").val();
+                                localStorage.setItem("task12", t12);
 
-                //$("#t12").val(t12);
+                            })
 
-                //$("#t1").val(t1);
+                                $("#save1").on("click",
+                                    function () {
+                                        var t1 = $("#t1").val();
+                                        localStorage.setItem("task1", t1);
+                                    })
 
-                //$("#t2").val(t2);
+                                        $("#save2").on("click",
+                                            function () {
+                                                var t2 = $("#t2").val();
+                                                localStorage.setItem("task9", t2);
+                                            })
 
-                //$("#t3").val(t3);
+                                                $("#save3").on("click",
+                                                    function () {
+                                                        var t3 = $("#t3").val();
+                                                        localStorage.setItem("task9", t3);
+                                                    })
 
-                //$("#t4").val(t4);
+                                                        $("#save4").on("click",
+                                                            function () {
+                                                                var t4 = $("#t4").val();
+                                                                localStorage.setItem("task9", t4);
+                                                            })
 
-                //$("#t5").val(t5);
+                                                                $("#save5").on("click",
+                                                                    function () {
+                                                                        var t5 = $("#t5").val();
+                                                                        localStorage.setItem("task9", t5);
 
-               
-               // localStorage.setItem("", t10.value);
-                //localStorage.setItem("", t11.value);
-                //localStorage.setItem("", t12.value);
-                //localStorage.setItem("", t1.value);
-                //localStorage.setItem("", t2.value);
-                //localStorage.setItem("", t3.value);
-                //localStorage.setItem("", t4.value);
-                //localStorage.setItem("", t5.value);
-
-
-           
-
-
-
-
-
-
-
-
-
+                                                                    })
+                                                                
